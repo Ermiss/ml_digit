8d8424398a462ae293187d88020d68facd54a249
Ermiss <79766558+Ermiss@users.noreply.github.com>
2021-04-27 00:12:24 +0300

    Update ml_digit.py

--- ml_digit.py
+++ ml_digit.py
@@ -22,14 +22,6 @@ x = data_train
 x = x.drop(['label'], axis=1)
 y = data_train.label
 
-# data_test = pd.read_csv('data/mnist_test.csv')
-# x_test = data_test
-# x_test = x_test.drop(['label'], axis=1)
-# y_test = data_test.label
-#
-# x = concat([x_train, x_test])
-# y = concat([y_train, y_test])
-
 # scaling the training data
 #scaler = StandardScaler()
 #x= scaler.fit_transform(x)
@@ -37,10 +29,6 @@ scaler = Normalizer()
 x = scaler.fit_transform(x)
 x = pd.DataFrame(x)
 #x = x - x.mean()
-
-# scaling the testing data
-# x_test = scaler.fit_transform(x_test)
-#x_test = x_test - x_test.mean()
 
 epoch = 30
 splits = 5
@@ -59,8 +47,6 @@ for i, (train, test) in enumerate(kfold.split(x)):
     #model.compile(optimizer='SGD', loss='mean_squared_error', metrics=['accuracy'])
     #model.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.05, momentum=0.6), loss='mean_squared_error', metrics=['accuracy'])
-
-    #x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=30, train_size=0.2)
 
     #callbacks = [EarlyStopping(monitor='val_accuracy', patience=3)]
     history = model.fit(x.iloc[train], y.iloc[train], epochs=epoch, validation_data=(x.iloc[test], y.iloc[test]))
@@ -97,9 +83,8 @@ print(mean_loss)
 print(mean_val_loss)
 print(mean_accuracy)
 print(mean_val_accuracy)
-# Plot training and
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
+
+# Plot training and Validation
 epochs = range(1, len(mean_loss) + 1)
 plt.plot(epochs, mean_loss, 'y', label='Training loss')
 plt.plot(epochs, mean_val_loss, 'r', label='Validation loss')
@@ -110,8 +95,6 @@ plt.legend(["Train", 'Prediction'], loc='upper right')
 plt.show()
 
 # Plot training and validation accuracy values
-# acc = history.history['accuracy']
-# val_acc = history.history['val_accuracy']
 plt.plot(epochs, mean_accuracy, 'y', label='Training acc')
 plt.plot(epochs, mean_val_accuracy, 'r', label='Validation acc')
 plt.title('Training and Validation accuracy')
